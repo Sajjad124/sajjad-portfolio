1615ffb18ac594362b9de33a871b069299963572
Muhammad Umair <181370124@gift.edu.pk>
2022-12-07 19:27:39 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Contact></Contact>
+    </div>
+  );  
+};
+
+export default App;
