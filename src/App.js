8ae3d61056eaa7823955abf345eec3449fde4c95
Muhammad Umair <181370124@gift.edu.pk>
2022-12-08 20:39:16 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Home from "./Components/Home";
 import Contact from "./Components/Contact";
 import About from "./Components/About";
 import Services from "./Components/Services";
+// import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-      <Contact/>
-      <About/>
-      <Services/>
-    </div>
-  );  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Services" element={<Services />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
