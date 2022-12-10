37f3e85c1d14ca6c4567f3a7dbaff77d999dbf8f
Muhammad Umair <181370124@gift.edu.pk>
2022-12-11 00:01:49 +0500

    Navbar Creation react router dom

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../Images/logo.JPG";
+import logo from "../Images/logo2.png";
+
+import Navbar from "./Navbar";
 
 const header = () => {
   return (
@@ -8,6 +10,7 @@ const header = () => {
       <NavLink to="./">
         <img src={logo} alt="logo" width="250px" height="70" />
       </NavLink>
+      <Navbar></Navbar>
     </header>
   );
 };
