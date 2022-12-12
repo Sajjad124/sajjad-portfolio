bb19ef4ebafed1f8ed2e139d6d156b42dd0604ad
Muhammad Umair <181370124@gift.edu.pk>
2022-12-12 23:59:48 +0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,16 +3,30 @@ import { NavLink } from "react-router-dom";
 import logo from "../Images/logo2.png";
 
 import Navbar from "./Navbar";
+import styled from "styled-components";
 
 const header = () => {
   return (
-    <header>
+    <MainHeader>
+      <h2>Sajjad Saroya | ReactJs Developer</h2>
       <NavLink to="./">
-        <img src={logo} alt="logo" width="250px" height="70" />
+        {/* <img src={logo} alt="logo" width="250px" height="70" className="logo" /> */}
       </NavLink>
-      <Navbar></Navbar>
-    </header>
+      <Navbar />
+    </MainHeader>
   );
 };
 
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h2 {
+    text-decoration: none;
+    color: black;
+  }
+`;
 export default header;
